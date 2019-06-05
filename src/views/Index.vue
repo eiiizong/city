e98f2de58f12d50d42f57382e50fa1a92578de00
eiiizong <eiiizong@163.com>
2019-06-05 18:25:45 +0800

    first commit

---
+++ src/views/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    1212
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.home {
+  font-size: 20px;
+}
+</style>
+
